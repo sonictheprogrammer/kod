7422c1c3b94c5c75634ade038c84a8ddcf348159
sonictheprogrammer <92324146+sonictheprogrammer@users.noreply.github.com>
2023-06-14 20:55:23 -0400

    Update git.sh

--- kod/shell/git.sh
+++ kod/shell/git.sh
@@ -1,4 +1,4 @@
-git commit ./shell/git.sh
+
 echo "This is a Git test. If you wanna do something, go ahead and `sudo (manager) (file)` "
 wait
 echo "If you want to quit the operations of this program, Press Ctrl+C (^C)"
