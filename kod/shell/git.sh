61cd36ec6122482793c42a17cd23a7383442f4b0
sonictheprogrammer <92324146+sonictheprogrammer@users.noreply.github.com>
2023-06-29 14:17:53 -0400

    Update git.sh

--- kod/shell/git.sh
+++ kod/shell/git.sh
@@ -2,4 +2,4 @@ echo "This is a Git test. If you wanna do something, go ahead and do `sudo (mana
 wait
 echo "If you want to quit the operations of this program, Press Ctrl+C (^C)"
 wait
-echo "Otherwise, Enjoy."
+echo "Otherwise, Enjoy!"
