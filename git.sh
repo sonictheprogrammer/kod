93fb337e47a54285327aeff2ffce9ee0877bebf1
sonictheprogrammer <92324146+sonictheprogrammer@users.noreply.github.com>
2023-07-08 09:38:22 -0400

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,5 @@
+echo "This is a Git test. If you wanna do something, go ahead and do `sudo (file)` "
+wait
+echo "If you want to quit the operations of this program, Press Ctrl+C (^C)"
+wait
+echo "Otherwise, Enjoy!"
