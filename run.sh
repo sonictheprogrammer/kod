cae5d076a34db8cbcf87fee8b763112d34d9dfcc
sonictheprogrammer <92324146+sonictheprogrammer@users.noreply.github.com>
2023-07-10 12:23:11 -0400

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,34 @@
+echo "Running Kod..."
+wait
+wait
+wait
+echo "Checking if files ran correctly..."
+cd kod
+wait
+echo "cd is done."
+ls
+wait
+echo "ls is done"
+wait
+echo "Done!"
+wait
+wait
+wait
+echo "Welcome to Kod! "
+echo "To check files, type in ls, as shown below,"
+ls
+echo "To edit, use nano, or your desired editor."
+wait
+echo "This is a test for Git, Credits to Linus and Junio!"
+wait
+echo "To quit, Press Ctrl+C (^C)."
+wait
+echo "If you enjoyed this, You can visit the GitHub Page, as shown below."
+wait
+wait 
+echo "https://github.com/sonictheprogrammer/kod"
+wait
+wait
+echo "Quiting..."
+cd
+
