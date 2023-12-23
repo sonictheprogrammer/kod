a13fb08b3b378ce4a11546f8e26e086d02420d2e
sonictheprogrammer <92324146+sonictheprogrammer@users.noreply.github.com>
2023-12-23 08:22:24 -0800

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,8 @@
+echo "Hi! Run this before running run.sh,"
+echo "This program will check if Kod is installed correctly."
+echo "this program will run all apps (not for run.sh) to check."
+./rpm.sh
+./win.sh
+./mac.sh
+./git.sh
+echo "Done!"
